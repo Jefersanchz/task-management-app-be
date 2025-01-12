de988de5c777ab2fa3da52f0fc5b72a569d603a1
Jefersson Sanchez <sanchezjs901@gmail.com>
2025-01-12 10:21:18 -0500

    Corregir el mapeo de las tareas por columnas

--- src/main/java/task_management_app/mapper/TaskMapper.java
+++ src/main/java/task_management_app/mapper/TaskMapper.java
@@ -1,6 +1,7 @@
 package task_management_app.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import task_management_app.dto.TaskDTO;
 import task_management_app.entity.TaskEntity;
@@ -9,6 +10,9 @@ import task_management_app.entity.TaskEntity;
 public interface TaskMapper {
     TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
 
+    @Mapping(source = "column.id", target = "columnId") 
     TaskDTO toDTO(TaskEntity task);
+
+    @Mapping(source = "columnId", target = "column.id") 
     TaskEntity toEntity(TaskDTO taskDTO);
 }
